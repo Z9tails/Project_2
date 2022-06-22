67b2ecbec6e8b79256b08b8c0d4e36ddc30b24c8
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-06-22 12:37:10 -0700

    Created basic DB, more updates to come

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO player (player_id, player_user_name)
+VALUES
+    ('');
+
+INSERT INTO cards (card_id, card_prompts, items_id)
+VALUES
+    ('');
+
+INSERT INTO items (items_id, items_name, items_description)
+VALUES
+    ('')
+
+
