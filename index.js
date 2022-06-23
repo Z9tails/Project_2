1443dee7148eff052b9f1efc14ec63f85c02c129
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-06-23 13:20:00 -0700

    created the basic main player and added temp backround style

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+// setting up the canvas elements
 const canvas = document.querySelector('canvas');
-
 const c = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-console.log(c);
+// begin main player class
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        };
+        this.width = 30;
+        this.height = 30;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+};
+
+const player = new Player();
+player.draw();
+
