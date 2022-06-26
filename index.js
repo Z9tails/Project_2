1596a6b24ed05df1f32a5f11fb85d076e2427035
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-06-26 12:18:41 -0700

    added Platform collision detection

--- index.js
+++ index.js
@@ -40,7 +40,25 @@ class Player {
     }
 };
 
+class Platform {
+    constructor() {
+        this.position = {
+            x: 200,
+            y: 100
+        } 
+        this.width = 200;
+        this.height = 20;
+    }
+
+    draw() {
+        c.fillStyle = 'blue';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
 const player = new Player();
+
+const platform = new Platform();
 
 const keys = {
     right: {
@@ -57,12 +75,22 @@ function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+    platform.draw()
 
     if (keys.right.pressed) {
         player.velocity.x = 2;
     } else if (keys.left.pressed) {
         player.velocity.x = -2;
     } else player.velocity.x = 0;
+
+    // Platform collision detection
+    if (player.position.y + player.height <= platform.position.y && 
+        player.position.y + player.height + player.velocity.y >= platform.position.y && 
+        player.position.x + player.width >= platform.position.x &&
+        player.position.x <= platform.position.x + platform.width
+    ) {
+        player.velocity.y = 0;
+    }
 };
 
 animate();
