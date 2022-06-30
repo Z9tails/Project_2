5da4f86d41a62219db7cc54a311710cbbba99751
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-06-29 19:10:09 -0700

    made a small change

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'K3wl!o05',
+        password: '',
         database: 'card_game'
     },
     console.log('Connected to the card_game database.')
