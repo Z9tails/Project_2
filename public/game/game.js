feff2c475866330b508e9940d805f40cb359d45b
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-07-06 00:46:10 -0700

    added fish image

--- public/game/game.js
+++ public/game/game.js
@@ -22,7 +22,7 @@ function preload ()
 {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
+    this.load.image('star', 'assets/fish.png');
     this.load.image('bomb', 'assets/bomb.png');
     // this.load.spritesheet('dude', 
     //     'assets/dude.png',
