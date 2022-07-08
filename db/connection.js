0bd148bb28d47c27732d72243b8f05ffc384dfe9
Z9tails <100103488+Z9tails@users.noreply.github.com>
2022-07-07 19:36:17 -0700

    working on the comments

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,15 @@
-const mysql = require('mysql2');
 
-// Connect to the database
-const db = mysql.createConnection(
-    {
-        host: 'localHost',
-        // Your MySQL username,
-        user: 'root',
-        // Your MySQL password
-        password: 'K3wl!o05',
-        database: 'card_game'
-    },
-    console.log('Connected to the card_game database.')
-); 
+// // Connect to the database
+// const db = mysql.createConnection(
+//     {
+//         host: 'localHost',
+//         // Your MySQL username,
+//         user: 'root',
+//         // Your MySQL password
+//         password: 'Smacktime1',
+//         database: 'card_game'
+//     },
+//     console.log('Connected to the card_game database.')
+// ); 
 
-module.exports = db;
+// module.exports = db;
