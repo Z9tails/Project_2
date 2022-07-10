4297251dad9cb53bce9e289b3ae265a2c3af7fc8
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-07-10 14:28:25 -0700

    made updates to styles

--- index.js
+++ index.js
@@ -3,14 +3,35 @@ const db = require("./config/connection");
 const htmlRoutes = require('./routes/htmlRoutes')
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-const PORT = process.env.PORT || 3001
-const app = express();
 
-app.use(express.urlencoded({ extended: true}));
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 // app.use('/html', htmlRoutes);
@@ -23,23 +44,8 @@ app.use(express.static('public'));
 }); */
 
 
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-app.use(session(sess));
 
-const helpers = require("./utils/helpers");
 
-const hbs = exphbs.create({ helpers });
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 app.use(require("./controllers/"));
 
