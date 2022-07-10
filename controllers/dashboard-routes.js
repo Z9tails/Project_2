7e9e53a640e92aa127e180d30294bc894ab8493d
Z9tails <100103488+Z9tails@users.noreply.github.com>
2022-07-10 14:38:13 -0700

    added eventhandler

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,22 +3,22 @@ const { User, Post, Comment } = require('../../models');
 
 
 // get all users
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   User.findAll({
-    attributes: { exclude: ['password'] }
+    attributes: { exclude: ["password"] },
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   User.findOne({
-    attributes: { exclude: ['password'] },
+    attributes: { exclude: ["password"] },
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
     include: [
       {
@@ -27,74 +27,70 @@ router.get('/:id', (req, res) => {
       {
         model: Comment,
       },
-    ]
+    ],
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res.status(404).json({ message: "No user found with this id" });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.post('/', (req, res) => {
-
+router.post("/", async (req, res) => {
   User.create({
     username: req.body.username,
-    password: req.body.password
+    password: req.body.password,
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       req.session.save(() => {
-        req.session.userId = dbUserData.id
+        req.session.userId = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
-        console.log(dbUserData)
+        console.log(dbUserData);
         res.json(dbUserData);
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.post('/login', (req, res) => {
-
+router.post("/login", async (req, res) => {
   User.findOne({
     where: {
-      username: req.body.username
+      username: req.body.username,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that id" });
+      return;
     }
-  })
-    .then(dbUserData => {
 
-      if (!dbUserData) {
-        res.status(400).json({ message: 'No user with that id' });
-        return;
-      }
+    const validPassword = dbUserData.checkPassword(req.body.password);
 
-      const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
 
-      if (!validPassword) {
-        res.status(400).json({ message: 'Incorrect password!' });
-        return;
-      }
+    req.session.save(() => {
+      req.session.userId = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
-      req.session.save(() => {
-        req.session.userId = dbUserData.id
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-      });
+      res.json({ user: dbUserData, message: "You are now logged in!" });
     });
+  });
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', async (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -105,7 +101,7 @@ router.post('/logout', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
@@ -127,20 +123,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   User.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(dbUserData => {
+    .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res.status(404).json({ message: "No user found with this id" });
         return;
       }
       res.json(dbUserData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,8 +3,8 @@ const path = require('path');
 const router = require('express').Router();
 
 // This has to be on the top than the other routers
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // If no matching route is found default to home page
