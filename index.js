5618e8443937ed742c2ff6cd6ee6c12ffaa91230
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-07-10 19:16:04 -0700

    Moved seeds folder from public

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ const sess = {
   }),
 };
 
-app.use(session(sess));
-
 const helpers = require("./utils/helpers");
 
 const hbs = exphbs.create({ helpers });
+
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
