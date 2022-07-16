62b3b1ce535959a2425ab0111a5c8dec4181ea9c
Z9tails <100103488+Z9tails@users.noreply.github.com>
2022-07-16 11:23:10 -0700

    Merge branch 'main' into authorization

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const fs = require("fs");
 const Sequelize = require('sequelize');
 
 console.log(process.env.DB_user)
@@ -6,10 +7,18 @@ console.log(process.env.DB_user)
 // create connection to db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_name, process.env.DB_user, process.env.DB_password, {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    });
+  : new Sequelize(
+      process.env.DB_name,
+      process.env.DB_user,
+      process.env.DB_password,
+      {
+        host: "hangover.mysql.database.azure.com",
+        dialect: "mysql",
+        port: 3306,
+        ssl: {
+          ca:fs.readFileSync('DigiCertGlobalRootCA.crt.pem')
+      }
+      }
+    );
 
 module.exports = sequelize;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
-DROP DATABASE IF EXISTS blog_db;
+-- Active: 1657995525096@@hangover.mysql.database.azure.com@3306
+DROP DATABASE IF EXISTS blog_db;  
 
 CREATE DATABASE blog_db;
